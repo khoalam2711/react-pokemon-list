a93727c40fc84b9212441da42d4b554d8149a557
KoahF <khoakg2013@gmail.com>
2023-03-28 20:40:36 +0700

    Add filter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { getPokemonTypes } from './api';
+import Filter from './components/filter';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [filterIsLoading, setFilterIsLoading] = useState(true);
+	const [activeFilters, setActiveFilters] = useState<Object>({});
+	const [availableFilters, setAvailableFilters] = useState<string[]>([]);
+
+	useEffect(() => {
+		const getData = async () => {
+			const { data } = await getPokemonTypes();
+			console.log(data);
+			const pokemonTypes: string[] = data.results.map((type: any) => type.name);
+			setAvailableFilters(pokemonTypes);
+			setFilterIsLoading(false);
+		};
+		getData();
+	});
+
+	const handleAddFilter = (filter: string) => {
+		setActiveFilters({ ...activeFilters, [filter]: true });
+	};
+
+	const handleRemoveFilter = (filter: string) => {
+		setActiveFilters({ ...activeFilters, [filter]: false });
+	};
+	return (
+		<div className="App">
+			<div className="mx-auto max-w-screen-xl">
+				{filterIsLoading ? null : (
+					<>
+						<Filter
+							filters={availableFilters}
+							activeFilters={activeFilters}
+							onAddFilter={handleAddFilter}
+							onRemoveFilter={handleRemoveFilter}
+						/>
+						<div className="my-12 mx-4 font-bold">1200 results found.</div>
+					</>
+				)}
+			</div>
+
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/filter.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+interface FilterProps {
+	filters: string[];
+	activeFilters: any;
+	onAddFilter: Function;
+	onRemoveFilter: Function;
+}
+
+const Filter = ({ filters, activeFilters, onAddFilter, onRemoveFilter }: FilterProps) => {
+	return (
+		<div className="flex items-center mx-4 my-4">
+			<div className="mr-2 my-4 font-bold self-start">Types:</div>
+			<div>
+				{filters.map((filter) =>
+					activeFilters[filter] ? (
+						<button
+							className="px-2 py-2 mx-2 my-2 border-red-900 border-2 rounded-md font-bold text-white bg-red-900"
+							key={filter}
+							onClick={() => onRemoveFilter(filter)}
+						>
+							{filter}
+						</button>
+					) : (
+						<button
+							className="px-2 py-2 mx-2 my-2 border-red-900 border-2 rounded-md font-bold text-red-900"
+							key={filter}
+							onClick={() => onAddFilter(filter)}
+						>
+							{filter}
+						</button>
+					)
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default Filter;
